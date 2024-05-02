71353eb6d93e146e2886964624fe8319dcda1eb8
Dev Kraken <soman@devkraken.com>
2024-05-03 02:49:31 +0400

    Listing Zod Schema

---
+++ zod/ListingSchema.ts
@@ -0,0 +1,41 @@
+import * as z from "zod";
+
+export const ListingSchema = z.object({
+  street: z.string().min(1, {
+    message: "Street is required.",
+  }),
+  city: z.string().min(1, {
+    message: "City is required.",
+  }),
+  stateId: z.number().min(1, {
+    message: "State is required",
+  }),
+  zipcode: z.string().min(1, {
+    message: "Zip Code is required",
+  }),
+  propertyType: z.string().min(1, {
+    message: "Property Type is required",
+  }),
+  price: z.number().positive().min(1, {
+    message: "Price is required",
+  }),
+  bedrooms: z.number().min(1, {
+    message: "Bedrooms is required",
+  }),
+  bathrooms: z.number().min(1, {
+    message: "Bathrooms is required",
+  }),
+  squareFootage: z.number().positive().min(1, {
+    message: "Square Footage is required",
+  }),
+  lotSize: z.number().positive().min(1, {
+    message: "Lot Size is required",
+  }),
+  yearBuilt: z.number().min(1, {
+    message: "Year Built is required",
+  }),
+  countryId: z.number().min(1, {
+    message: "Country is required",
+  }),
+  description: z.string().min(0),
+});
